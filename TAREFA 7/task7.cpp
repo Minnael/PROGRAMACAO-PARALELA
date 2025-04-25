ba41f5014efd37ea754551819f5ef5dde5a47b2a
Minnael <minaelcampelo3@gmail.com>
2025-04-24 23:02:57 -0300

    end task7

--- TAREFA 7/task7.cpp
+++ TAREFA 7/task7.cpp
@@ -1,51 +1,57 @@
-#include <iostream>
-#include <string>
-#include <omp.h>
+#include <iostream>    
+#include <string>      
+#include <omp.h>       
 
-using namespace std;
+using namespace std;   
 
-// Definição do nó da lista encadeada
-struct Node {
-    string filename;
-    Node* next;
-    Node(const string& name) : filename(name), next(nullptr) {}
+struct No {
+    string nomeArquivo; 
+    No* proximo;        
+
+    No(const string& nome) {
+        nomeArquivo = nome;
+        proximo = nullptr;
+    }
 };
 
 int main() {
-    // 1) Construir a lista encadeada com arquivos fictícios
-    const int N = 8;
-    Node* head = new Node("file1.txt");
-    Node* tail = head;
-    for (int i = 2; i <= N; ++i) {
-        tail->next = new Node("file" + to_string(i) + ".txt");
-        tail = tail->next;
+    const int N = 20; 
+
+    No* inicio = new No("ARQUIVO-1.txt");               
+    No* cauda = inicio;   
+
+    for (int i = 2; i <= N; ++i) {                      
+        cauda->proximo = new No("ARQUIVO" + to_string(i) + ".txt"); 
+        cauda = cauda->proximo;                         
     }
 
-    // 2) Região paralela para criação e execução de tarefas
-    #pragma omp parallel
+
+    #pragma omp parallel                              // INICIA REGIÃO PARALELA (VÁRIAS THREADS PODEM EXECUTAR ISSO)
     {
-        #pragma omp single nowait
+        #pragma omp single nowait                     // APENAS UMA THREAD EXECUTA O BLOCO, SEM BARREIRA AO FINAL
         {
-            Node* p = head;
-            while (p) {
-                #pragma omp task firstprivate(p)
+            No* p = inicio;          
+            while (p) {                               // ENQUANTO HOUVER NÓS NA LISTA
+                #pragma omp task firstprivate(p)      // CRIA UMA TASK QUE CAPTURA 'P' POR VALOR
                 {
-                    int tid = omp_get_thread_num();
-                    cout << "Thread " << tid
-                         << " processou " << p->filename << endl;
+                    int tid = omp_get_thread_num();   // OBTÉM O ID DA THREAD QUE EXECUTA A TASK
+                    #pragma omp critical
+                    {
+                        cout << "Thread " << tid << ": processou: " << p->nomeArquivo << endl;
+                    }
                 }
-                p = p->next;
+                p = p->proximo;                       
             }
-            #pragma omp taskwait
+            #pragma omp taskwait                      // AGUARDA TODAS AS TASKS GERADAS NESTA REGIÃO
         }
     }
 
-    // 3) Liberar memória
-    while (head) {
-        Node* tmp = head;
-        head = head->next;
-        delete tmp;
+    
+    while (inicio) {                                  // ENQUANTO HOUVER NÓS PARA LIBERAR
+        No* tmp = inicio;                             // GUARDA O NÓ ATUAL EM 'TMP'
+        inicio = inicio->proximo;                     // MOVE 'INICIO' PARA O PRÓXIMO NÓ
+        delete tmp;                                   // DESALOCA O NÓ GUARDADO EM 'TMP'
     }
 
-    return 0;
+    return 0;                                         
 }
